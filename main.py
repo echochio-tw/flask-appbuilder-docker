e37f4110f5af62666a7a930a22172740bb98c890
echochio <echochio-tw@users.noreply.github.com>
2023-06-30 16:40:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from app import app
 
 if __name__ == "__main__":
         app.run(debug=True, port=80)
+
+#GCP cloud run
+#if __name__ == "__main__":
+#    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))        
